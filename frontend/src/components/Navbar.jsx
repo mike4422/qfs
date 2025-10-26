70a189ea1d8f5dd6b4110254d1d99eb94f687e4d
mike clinton <mikeclinton508@gmail.com>
2025-10-26 08:34:51 +0100

    image fix

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -38,7 +38,7 @@ export default function Navbar() {
         {/* Brand */}
        <Link to="/" className="flex items-center space-x-2">
   <img
-    src="/frontend/public/qfs-logo-b.png"
+    src="/qfs-logo-b.png"
     alt="QFS Worldwide Logo"
     className="w-20 h-20 "
   />
