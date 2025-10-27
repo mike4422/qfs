bd10fdfb1d6545300878116d79833d16d6641272
mike clinton <mikeclinton508@gmail.com>
2025-10-27 04:27:37 +0100

    added talkto

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar.jsx";
 import Footer from "./Footer.jsx";
+import TalkToWidget from "./components/TalkToWidget";
 
 export default function Layout() {
   return (
@@ -9,6 +10,7 @@ export default function Layout() {
       <main className="flex-1">
         <Outlet />
       </main>
+       <TalkToWidget />
       <Footer />
     </div>
   );
